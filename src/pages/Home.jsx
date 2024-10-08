3bde7ff984bd37ca6bdff7347b497647bdc26ba0
David820 <dwddto69@gmail.com>
2024-10-09 01:43:04 +0200

    modif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,57 @@
 import { css } from "@emotion/css";
 import imLdg from "../assets/img/ldgpge.webp";
 import imCmp from "../assets/img/cmpnt.webp";
+import imGglchrm from "../assets/img/chrom.webp";
+import imYtbvdo from "../assets/img/ytb.webp";
 
 const Home = () => 
 
-<div className={css`display:grid;place-items:center;height:83dvh;`}>
+<div className={css`display:grid;place-items:center;height:83dvh;@media (width <= 1200px) {height:unset;}`}>
     <div className={css`
         width:100%;
         display:flex;
         justify-content:space-between;
+        @media (width <= 1200px) {flex-direction:column;align-items:center;img{margin-top:56px;}}
     `}>
     
         <img src={imLdg} width={240} />
     
         <div className={css`
-        width:100%;max-width:720px;display:flex;flex-direction:column;gap:24px;padding:0 16px;position:relative;
-            h2{font-size:56px;line-height:1;}
-            a{position:absolute;bottom:0;background:#68a0c1;padding:16px;color:white;}
+            width:100%;max-width:720px;display:flex;flex-direction:column;gap:32px;padding:0 16px;position:relative;
+            h2{font-size:48px;line-height:1;font-family:"Caprasimo";font-weight:100;}
+            p{width:560px;}
+            a{background:#68a0c1;padding:16px;color:white;}
+            @media (width <= 1200px) {
+            text-align:center;
+            h2{font-size:32px;margin-top:56px;}
+            p{width:100%;margin-bottom:16px;}
+            a{align-self:center;}
+            }
         `}>
             <h2>Une sélection de reportages et documentaires captivants</h2>
             <p>Plongez au cœur des grands débats, des enjeux actuels, et des personnalités qui façonnent l'avenir de notre société.</p>
-            <small> Tous nos contenus sont hébergés sur YouTube pour un accès rapide et facile.</small>
-            <a href="/videos">Voir les vidéos</a>
+            <div className={css`
+                display:flex;
+                align-items:flex-end;
+                justify-content:space-between;
+                gap:32px;
+                position:absolute;
+                bottom:0;
+                width:100%;
+                @media (width <= 1200px) {position:unset;flex-direction:column;align-items:flex-start;}
+                `}>
+                <a href="/videos">Voir les vidéos</a>
+                <div className={css`
+                    display:flex;
+                    align-items:flex-end;
+                    gap:16px;
+                    translate: 0 5px;
+                    @media (width <= 1200px) {align-items:center;justify-content:center;width:100%;}
+                `}>
+                    <img src={imGglchrm} width={40} alt="Google Chrome" />
+                    <img src={imYtbvdo} width={152} alt="Youtube" />
+                </div>
+            </div>
         </div>
 
         <img src={imCmp} width={200} />
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,10 +1,210 @@
+import { createSignal } from "solid-js";
 import { css } from "@emotion/css";
 
-const Videos = () => 
+const Videos = () => {
+  const [currentVideo, setCurrentVideo] = createSignal(""); 
+  const [searchTerm, setSearchTerm] = createSignal(""); 
 
-<div>
-    <h1>Vidéos</h1>
+  const videos = [
 
-</div>
+    { title: "Création de l'État d'Israël", imgSrc: "https://img.youtube.com/vi/oOJvNxFiC4M/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/oOJvNxFiC4M?si=yrcRd90OBX3tGTkP&amp;controls=0" },
+
+    { title: "Mossad : l'histoire secrète d'Israël", imgSrc: "https://img.youtube.com/vi/1EFr3Y7eQMA/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/1EFr3Y7eQMA?si=5HLkJXf8R-RO6YAQ&amp;controls=0" },
+
+    { title: "La face cachée du recyclage", imgSrc: "https://img.youtube.com/vi/Iv9icf-0wJ4/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/Iv9icf-0wJ4?si=iL1JVMlgrqT72hOu&amp;controls=0" },
+
+    { title: "Maastricht, 30 ans après", imgSrc: "https://img.youtube.com/vi/KlrIvPz5wDA/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/KlrIvPz5wDA?si=3Wzq_TS4KpsahOau&amp;controls=0" },
+
+    { title: "Services publics, cash investigation", imgSrc: "https://img.youtube.com/vi/PyiSyO5J0bc/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/PyiSyO5J0bc?si=2MgV8YIBZJgfESL5&amp;controls=0" },
+
+    { title: "L'épargne des Français : le scandale de l'argent caché", imgSrc: "https://img.youtube.com/vi/hpqhSsC6Suc/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/hpqhSsC6Suc?si=HnpIF4W4NrKtI_8t&amp;controls=0" },
+
+    { title: "Les enfants terrible de la gauche, parti socialiste", imgSrc: "https://img.youtube.com/vi/PDs9nmqQNkk/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/PDs9nmqQNkk?si=Yj2dWJNFLknyNWEw&amp;controls=0" },
+
+    { title: "Un virus s'est-il échappé du Laboratoire P4 de Wuhan ", imgSrc: "https://img.youtube.com/vi/EYpgcOz2OWs/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/EYpgcOz2OWs?si=LCJEgGJU1VnesKo7&amp;controls=0" },
+
+    { title: "L'Intérieur : Au coeur des crises", imgSrc: "https://img.youtube.com/vi/tTD9SAECPZ0/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/tTD9SAECPZ0?si=oGZuYNz7g6nxwx1S&amp;controls=0" },
+
+    { title: "Poutine et les présidents français", imgSrc: "https://img.youtube.com/vi/DgTkt4pUBcs/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/DgTkt4pUBcs?si=yiIT5kLRW4ez-SEt&amp;controls=0" },
+
+    { title: "Nucléaire, un fiasco français", imgSrc: "https://img.youtube.com/vi/bYGoGoJe89Y/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/bYGoGoJe89Y?si=efnUNc41HWv-ICnj&amp;controls=0" },
+
+    { title: "Au cœur de la présidentielle", imgSrc: "https://img.youtube.com/vi/bySNCiODilE/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/bySNCiODilE?si=q2-VfTDxcEB4fDIZ&amp;controls=0" },
+
+    { title: "Macron, en marche vers l élysée", imgSrc: "https://img.youtube.com/vi/xaCxgo5r_xE/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/xaCxgo5r_xE?si=_fzJSAz8uV0NBeJ4&amp;controls=0" },
+
+    { title: "ena", imgSrc: "https://img.youtube.com/vi/gjv15p9IBNE/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/gjv15p9IBNE?si=miU2IHaYSuCs6tac&amp;controls=0" },
+
+    { title: "Nos très chers députés : dans les coulisses de l'Assemblée nationale", imgSrc: "https://img.youtube.com/vi/c-9bfpoRA0U/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/c-9bfpoRA0U?si=XuSL_Rctq2PEWYs_&amp;controls=0" },
+
+    { title: "Opération Rubicon : espionnage à l'échelle mondiale", imgSrc: "https://img.youtube.com/vi/Wm1Vk90tUKw/hqdefault.jpg", videoUrl: "https://www.youtube.com/embed/Wm1Vk90tUKw?si=4Uo8OsaO6b1adpgB&amp;controls=0" },
+
+  ];
+
+  const filteredVideos = () => {
+    return videos.filter(video =>
+      video.title.toLowerCase().includes(searchTerm().toLowerCase())
+    );
+  };
+
+  const resetSearch = () => {
+    setSearchTerm("");
+  };
+
+  const blockScroll = () => {
+    document.body.style.overflow = "hidden";
+  };
+
+  const enableScroll = () => {
+    document.body.style.overflow = "";
+  };
+
+  const handleVideoClick = (url) => {
+    setCurrentVideo(url);
+    blockScroll();
+  };
+
+  const closeIframe = () => {
+    setCurrentVideo("");
+    enableScroll();
+  };
+
+  return (
+    <div>
+      <div
+        className={css`
+          min-height: 100dvh;
+          margin-top: 80px;
+        `}
+      >
+        <div
+          className={css`
+            margin-bottom: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          <input
+            type="text"
+            placeholder="Rechercher une vidéo"
+            value={searchTerm()}
+            onInput={(e) => setSearchTerm(e.target.value)}
+            className={css`
+              padding: 11px;
+              width: 320px;
+              margin-right: 16px;
+              border: 1px solid #555;
+              border-radius: 4px;
+              outline:0;
+              font-size:18px;
+              &::placeholder{color:#999;font-size:16px;letter-spacing:1px;font-family: "Afacad Flux", sans-serif;font-weight:300;font-style:italic;}
+              `}
+              />
+          <button
+            onClick={resetSearch}
+            className={css`
+              padding: 12px;
+              border: 1px solid #555;
+              cursor: pointer;
+              border-radius: 4px;
+              font-size:16px;
+              color:#555;
+              font-weight:500;transition:300ms;
+              &:hover{transition:200ms;border: 1px solid tomato;color:tomato;}
+            `}
+          >
+            X
+          </button>
+        </div>
+
+        <div className={css`
+            display:flex;
+            flex-wrap:wrap;
+            justify-content:center;
+            gap:32px;
+            margin:80px auto 120px;
+            width:100%;
+            max-width:960px;  
+        `}>
+          {filteredVideos().length > 0 ? (
+            filteredVideos().map((video) => (
+              <img
+                src={video.imgSrc}
+                alt={video.title}
+                title={video.title}
+                width={200}
+                className={css`
+                  cursor: pointer;transition:300ms;
+                  &:hover{translate:0 -6px;scale:1.05;transition:200ms;}
+                `}
+                onClick={() => handleVideoClick(video.videoUrl)}
+              />
+            ))
+          ) : (
+            <p
+              className={css`
+                color: tomato;
+                font-size:24px;
+                font-weight:500;
+              `}
+            >
+              Aucun résultat
+            </p>
+          )}
+        </div>
+
+        {currentVideo() && (
+          <div
+            className={css`
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              background-color: #00000099;
+              display: grid;
+              place-content: center;
+              backdrop-filter: blur(8px);
+            `}
+          >
+            <button
+              onClick={closeIframe}
+              className={css`
+                position: absolute;
+                top: 16px;
+                right: 16px;
+                padding: 12px;
+                background-color: #999;
+                border: none;
+                cursor: pointer;
+                font-size: 20px;
+                font-weight:600;
+                z-index: 99;transition:300ms;
+                &:hover{background-color:#CCC;transition:200ms;}
+              `}
+            >
+              X
+            </button>
+
+            <iframe
+              src={currentVideo()}
+              className={css`
+                width: 880px;
+                height: 480px;
+                @media (width <= 1200px) {width:100%;max-width:560px;min-width:340px;height:auto;min-height:220px}              
+              `}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              title="YouTube Video"
+              referrerpolicy="strict-origin-when-cross-origin"
+              loading="lazy" 
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Videos;
