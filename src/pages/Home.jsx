066d5fa3d4522186adf9036807a0273c87861d3b
David820 <dwddto69@gmail.com>
2024-10-13 22:53:20 +0200

    modif

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { css } from "@emotion/css";
 import imLdg from "../assets/img/ldgpge.webp";
 import imCmp from "../assets/img/cpnt.webp";
-import imGglchrm from "../assets/img/chrom.webp";
-import imYtbvdo from "../assets/img/ytb.webp";
+import imChrom from "../assets/img/chrom.webp";
+import imYtbe from "../assets/img/ytbe.webp";
+import imGgle from "../assets/img/ggle.webp";
 
 const Home = () => 
 
@@ -14,18 +15,18 @@ const Home = () =>
         @media (width <= 1200px) {flex-direction:column;align-items:center;img{margin-top:56px;}}
     `}>
     
-        <img src={imLdg} width={240} />
+        <img className={css`@media (orientation:portrait) {width:clamp(300px,100%,400px);}`} src={imLdg} width={240} />
     
         <div className={css`
-            width:100%;max-width:720px;display:flex;flex-direction:column;gap:32px;padding:0 16px;position:relative;
+            width:clamp(300px,100%,720px);display:flex;flex-direction:column;gap:32px;padding:0 16px;position:relative;
             h2{font-size:48px;line-height:1;font-family:"Caprasimo";font-weight:100;}
-            p{width:400px;}
+            p{width:clamp(300px,100%,400px);}
             a{background:#68a0c1;padding:16px;color:white;}
             @media (width <= 1200px) {
-            text-align:center;
-            h2{font-size:32px;margin-top:56px;}
-            p{width:100%;margin-bottom:16px;}
-            a{align-self:center;}
+            padding:0;
+            h2{font-size:32px;margin-top:56px;text-align:left;}
+            p{margin-bottom:16px;line-height:1.1;}
+            a{align-self:left;}
             }
         `}>
             <h2>Une sélection de reportages et documentaires captivants</h2>
@@ -38,24 +39,24 @@ const Home = () =>
                 position:absolute;
                 bottom:0;
                 width:100%;
-                a{transition:300ms;&:hover {translate:0 -4px;transition:200ms;}}
+                a{transition:300ms;min-width:fit-content;&:hover {translate:0 -4px;transition:200ms;}}
                 @media (width <= 1200px) {position:unset;flex-direction:column;align-items:flex-start;}
                 `}>
                 <a href="/videos">Voir les vidéos</a>
-                <div className={css`
-                    display:flex;
-                    align-items:flex-end;
-                    gap:16px;
-                    translate: 0 5px;
-                    @media (width <= 1200px) {align-items:center;justify-content:center;width:100%;}
-                `}>
-                    <img src={imGglchrm} width={40} alt="Google Chrome" />
-                    <img src={imYtbvdo} width={152} alt="Youtube" />
-                </div>
             </div>
         </div>
 
-        <img src={imCmp} width={200} />
+        <aside className={css`position:absolute;top:16px;right:24px;display:flex;align-items:center;img:nth-of-type(3){margin-left:32px};@media (orientation: portrait) {top:32px;right:unset;
+            img{width:32px};
+            img:nth-of-type(2){margin-left:4px;width:28px;}
+            img:nth-of-type(3){width:120px;}
+        }`}>
+        <img src={imGgle} width={40} />
+        <img src={imChrom} width={35} />
+        <img src={imYtbe} width={190} />
+        </aside>
+
+        <img className={css`@media (orientation:portrait) {width:clamp(300px,100%,400px);}`} src={imCmp} width={200} />
     
     </div>
 </div>
